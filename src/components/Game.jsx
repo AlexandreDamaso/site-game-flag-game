7a0fd550e122cb9ede3f0fb6619a05f23ea744d8
Alexandre Damaso <alexandredamasocosta@gmail.com>
2025-11-05 16:02:03 -0300

    42 - Criando o JSX do jogo

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -19,12 +19,65 @@ function Game() {
    },[]);
 
    useEffect(() => {
-      console.log(flags);
+      if (Object.keys(flags).length > 0) {
+         startNewRound();
+      }
    },[flags]);
 
+   const startNewRound = () => {
+      const flagCodes = Object.keys(flags);
+      const selectedCodes = [];
+      while (selectedCodes.length < 3) {
+         const randomIndex = Math.floor(Math.random() * flagCodes.length);
+         const randomCode = flagCodes[randomIndex];
+         if (!selectedCodes.includes(randomCode)) {
+            selectedCodes.push(randomCode);
+         }
+      }
+
+      console.log(selectedCodes);
+
+      const correctIndex = Math.floor(Math.random() * selectedCodes.length);
+      const correctCode = selectedCodes[correctIndex];
+      setCorrectFlag(flags[correctCode]);
+      setFlagUrl('https://flagcdn.com/16x12/'+correctCode+'.png');
+      setOptions(selectedCodes.map(code => flags[code]));
+      setMessage('');
+      setAnswered(false);
+   }
+
+   const handleOptionClick = (selectedFlag) => {
+      setAnswered(true);
+      if (selectedFlag === correctFlag) {
+         setMessage('Acertou!');
+         setCorrectCount(correctCount + 1);
+      } else {
+         setMessage('Errou!')
+         setIncorrectCount(incorrectCount + 1);
+      }
+
+      setTimeout(() => {
+         startNewRound();
+      }, 2000);
+   }
+
    return (
-      <div>
-         <Flag />
+      <div className='game-container'>
+         <h1 className='game-title'>De onde é essa bandeira?</h1>
+         <div className="score-container">
+            <p className="score-correct">Acertos: {correctCount}</p>
+            <p className={message == 'Acertou' ? 'score correct' : message == 'Errou' ? 'score wrong' : ''}>{message}</p>
+            <p className="score-wrong">Erros: {incorrectCount}</p>
+         </div>
+         {flagUrl ? <Flag flagUrl={flagUrl} /> : 'Carregando...'}
+         <div className="options-container">
+            {options.map((flag, index) => (
+               <button onClick={() => handleOptionClick(flag)} className={answered && flag === correctFlag ? 'option-button correct-option' : 'option-button'} key={index}>
+                  {index + 1}) {flag}
+               </button>
+            ))}
+         </div>
+         {/* <Flag /> */}
       </div>
    )
 }
